3af4ee3f1f292b68632113aad933082ce873de9f
yosef <yose981@walla.com>
2019-08-07 19:30:48 +0300

    PcapReader fixed bug

--- DiscoveryRecorder/DiscoveryRecorder/PcapReader.cpp
+++ DiscoveryRecorder/DiscoveryRecorder/PcapReader.cpp
@@ -26,31 +26,17 @@ void PcapReader::read()
 	struct pcap_pkthdr* header;
 
 	const u_char* data = NULL;
-	/*
-	* Step 6 - Loop through packets and print them to screen
-	*/
 	unsigned int packetCount = 0;
 	while (int returnValue = pcap_next_ex(pcap, &header, &data) >= 0)
 	{
-		/*packet* p = new packet();
-		struct pcap_pkthdr* pHeader = new pcap_pkthdr();
-		const u_char* pData = new const u_char[header->caplen];
-
-		memcpy(pHeader, header, sizeof(struct pcap_pkthdr));
-		memcpy(&pData, &data, header->caplen);*/
-		
-
 		packet* p = new packet();
 		p->header = new pcap_pkthdr();
 		p->pkt_data = new const u_char[header->caplen];
 
 		*(p->header) = *header;
-		memcpy(&p->pkt_data, &data, header->caplen);
+		memcpy((void*)p->pkt_data, data, header->caplen);
 
-
-		for (int i = 0; i < p->header->caplen; i++) {
-			std::cout << p->pkt_data[i];
-		}
+		
 		delete p;
 	}
 }
